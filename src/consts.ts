7958bb6f31dcc6cb578c876a76f41eaf4fc2a202
Dodrio33 <josephshipleysmith@gmail.com>
2025-03-06 18:23:19 +0000

    Added Proper Title and Description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Binary Brigade';
+export const SITE_DESCRIPTION = 'The Binary Brigade enforces digital purity, eradicating hex believers to restore order under Supreme Bit Joseph Shipley Smith.';
