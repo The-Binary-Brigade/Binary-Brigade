e0bfea8bec758d80a115b7c49f7485f69f7a3afe
Moff Bit <moffbit@binarybrigade.xyz>
2025-03-11 12:58:51 +0000

    implement astro-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://binarybrigade.xyz',
-	integrations: [mdx(), sitemap()],
+    site: 'https://binarybrigade.xyz',
+    integrations: [mdx(), sitemap(), icon()],
 });
